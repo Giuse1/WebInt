de32f12052d2106d2fcbe76649218c428b8df3c0
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2019-11-27 22:55:28 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,21 +1,19 @@
-function takeUrl(e) {
-    var new_url = urlForm.value;
-    source.src = new_url;
+function takeUrl() {
+    source.src = urlForm.value;
     video.load();
     video.play();
 }
 
-function takeOffset(e) {
+function takeOffset() {
 
-    var new_offset = offset.value;
+    let new_offset = offset.value;
     console.log(typeof new_offset);
-    var number_offset = Number(new_offset);
+    let number_offset = Number(new_offset);
     if (typeof number_offset === 'number' && Number.isInteger(number_offset)){
-        var video = document.getElementById("myVideo");
         if(number_offset < 0)
             window.alert("Error: the number is negative");
         else if (number_offset > video.duration)
-            window.alert("Error: the number exceeds the lenght of the video");
+            window.alert("Error: the number exceeds the length of the video");
         else {
             video.currentTime = new_offset;
             video.play();
@@ -25,73 +23,108 @@ function takeOffset(e) {
 }
 
 function toggleControls() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("text");
-    var video = document.getElementById("myVideo");
+    let checkBox = document.getElementById("myCheck");
     if (checkBox.checked == true)
         video.controls = true;
     else
         video.controls =  false;
 }
 
+
 var video = document.getElementById("myVideo");
 var source = video.getElementsByTagName("source")[0];
-var urlForm  = document.getElementById("urlForm");
-urlForm.addEventListener("keydown", function (e){
-    if (e.keyCode === 13){
-        takeUrl(e);
-    }
-});
-
-var offset = document.getElementById("Offset");
-offset.addEventListener("keydown", function (e){
-    if (e.keyCode === 13){
-        takeOffset(e);
-    }
-});
-
-var angle = 0;
-var rotationButton = document.getElementById("rotationButton");
-rotationButton.addEventListener("click", function (e) {
-    angle += 90;
-    angle = angle%360;
-    video.style.transform = "rotate("+angle+"deg)";
-})
-
-source.addEventListener("error", function(e) {
-    window.alert("Error: the video link is wrong!")
-});
 
 
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();
+var numberClicks = 0;
+var canvas = document.getElementById("previewCanvas");
+canvas.width = 150;
+canvas.height = 150;
+
+video.addEventListener("canplay",function () {
+    var $this = this;
+    console.log("canplay");
+    console.log(video.readyState);
+    canvas.getContext('2d').drawImage($this, 0, 0, canvas.width, canvas.height);
 });
 
 
 var previewButton = document.getElementById("previewButton");
 previewButton.addEventListener("click",function (e) {
-    var canvas = document.createElement("canvas");
-    canvas.width = "200px";
-    canvas.height = "150px";
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-    var img = document.createElement("img");
-    img.src = canvas.toDataURL();
-    let url = URL.createObjectURL(img);
-    window.alert(url);
-    previewButton.setAttribute("data-content",url);
+    numberClicks += 1;
+
+    if (numberClicks%2 == 1){
+        canvas.style.display = "block";
+    }else
+        canvas.style.display = "none";
+});
+
+
+// change video URL when a new one is inserted
+var urlForm  = document.getElementById("urlForm");
+urlForm.addEventListener("keydown", function (e){
+    if (e.keyCode === 13){
+        takeUrl();
+    }
+});
+
+// offeset video
+var offset = document.getElementById("Offset");
+offset.addEventListener("keydown", function (e){
+    if (e.keyCode === 13){
+        takeOffset();
+    }
+});
+
+// rotate the video
+var angle = 0;
+var rotationButton = document.getElementById("rotationButton");
+rotationButton.addEventListener("click", function () {
+    angle += 90;
+    angle = angle%360;
+    video.style.transform = "rotate("+angle+"deg)";
 })
 
+// listener to check if the inserted URL is correct
+source.addEventListener("error", function() {
+    window.alert("Error: the video link is wrong!")
+});
 
-var testArray = ["Shirt", "Bottom", "Shoes"];
-var toAdd = "toAdd";
-window.sessionStorage.setItem("items", JSON.stringify(testArray));
-var storedArray = JSON.parse(sessionStorage.getItem("items"));//no brackets
-var i;
-var a = [];
-for (i = 0; i < storedArray.length; i++) {
-    a.push(storedArray[i]);
-}
-a.push(toAdd);
-for (i = 0; i < a.length; i++) {
-    console.log(a[i]);
-}
+// Mirroring the video in a canvas
+var mCanvas = document.getElementById('mirroredCanvas');
+var context = mCanvas.getContext('2d');
+
+
+video.addEventListener('loadedmetadata', function() {
+    mCanvas.width = 150;
+    mCanvas.height = 100
+});
+
+video.addEventListener('play', function() {
+    var $this = this;
+    (function loop() {
+        if (!$this.paused && !$this.ended) {
+            context.save();
+            context.scale(-1, 1);
+            context.drawImage($this, 0, 0, mCanvas.width*-1, mCanvas.height);
+            context.restore();
+            setTimeout(loop, 1000 / 30); // drawing at 30fps
+        }
+    })();
+}, 0);
+
+
+// form
+/*var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", function () {
+
+    let name = document.getElementById("nameInput").valueOf();
+    let lastName = document.getElementById("latNameInput").valueOf();
+    let email = document.getElementById("emailInput").valueOf(); // already checked
+    let phone = document.getElementById("phoneInput").valueOf();
+
+    window.sessionStorage.setItem("name", name);
+    window.sessionStorage.setItem("lastName", lastName);
+    window.sessionStorage.setItem("email", email);
+    window.sessionStorage.setItem("phone", phone);
+});*/
+
